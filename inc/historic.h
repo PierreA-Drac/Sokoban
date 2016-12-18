8588f69f65d2ada099bf92262e858f181c4386e4
PierreA-Drac <drac78@live.fr>
2016-12-18 23:52:02 +0100

    Create historic.c/.h files

---
+++ inc/historic.h
@@ -0,0 +1,22 @@
+/**
+ * # Nom ..........: AYOUB
+ * # Prénom .......: Pierre
+ * # N° étudiant ..: 21501002
+ *
+ * Ce fichier contient les fonctions permettant de manipuler l'historique
+ * d'un Sokoban.
+ */
+
+#ifndef __HISTORIC_H
+#define __HISTORIC_H
+
+#include "structs.h"
+#include "consts.h"
+
+/**
+ * ####  -----------------------------------------------------------------  ####
+ * ##	  Fonctions =====================================================     ##
+ * ####  -----------------------------------------------------------------  ####
+ */
+
+#endif
